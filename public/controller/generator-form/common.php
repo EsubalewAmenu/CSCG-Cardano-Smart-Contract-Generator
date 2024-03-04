af619f5c4eb251fe4bb321df21673e176eef1bf4
Esubalew <esubalew.a2009@gmail.com>
2024-03-04 05:36:01 +0300

    file stracturn improved

---
+++ public/controller/generator-form/common.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       https://github.com/EsubalewAmenu
+ * @since      1.0.0
+ *
+ * @package    CSCG
+ * @subpackage CSCG/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    CSCG
+ * @subpackage CSCG/public
+ * @author     Esubalew Amenu <esubalew.a2009@gmail.com>
+ */
+class CSCG_public_common
+{
+
+    $active_sc = array("NFT" => "nft", "Burn" => "burn", "Mint" => "mint");
+
+}
