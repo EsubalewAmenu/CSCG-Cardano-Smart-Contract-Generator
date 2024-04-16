25d298d7a64b2ae428665708e5f8e1e4311ea9e8
mina <minahussen45@gmail.com>
2024-04-16 23:02:57 +0300

    adding hide and show functionality

--- public/partials/smart-contracts/nft-burn/index.php
+++ public/partials/smart-contracts/nft-burn/index.php
@@ -5,24 +5,28 @@
         <input class="form-input" type="text" name="token_name" id="token_name" required>
     </div>
 </div>
-
-<div class="form-container">
-    <div class="input-parent">
+<div class="form-container checkbox-container owner-ref-address-ckeckbox">
+    <div class="checkbox-parent">
+        <input class="form-input form-checkbox owner-ref-address-ckeckbox" type="checkbox" name="owner_ref_address" id="owner_ref_address" required>
         <label class="form-label" for="owner_ref_address">Is Owner Ref Address static:</label>
-        <input class="form-input" type="checkbox" name="owner_ref_address" id="owner_ref_address" required>
-    </div>
+    
 </div>
 
-<div class="form-container">
+<div class="form-container owner-ref-address-container hidden">
     <div class="input-parent">
-        <label class="form-label" for="owner_ref_address">Owner Ref Address:</label>
+               <label class="form-label" for="owner_ref_address">Owner Ref Address:</label>
         <input class="form-input" type="text" name="owner_ref_address" id="owner_ref_address" required>
     </div>
 </div>
 
 <div class="form-container">
-    <div class="input-parent">
+    <div class="checkbox-parent">
+        <input class="form-input form-checkbox" type="checkbox" name="add_offchain_code" id="add_offchain_code" required>
         <label class="form-label" for="add_offchain_code">Add offchain code:</label>
-        <input class="form-input" type="checkbox" name="add_offchain_code" id="add_offchain_code" required>
     </div>
 </div>
+
+<script>
+    console.log(document.querySelector('#owner_ref_address'));
+    console.log('hello');
+</script>
