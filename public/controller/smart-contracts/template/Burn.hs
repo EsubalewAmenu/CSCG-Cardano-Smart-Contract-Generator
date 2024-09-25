595536815f46b6614b091b50557836463135014d
Esubalew <esubalew.a2009@gmail.com>
2024-09-25 14:44:20 +0300

    unneccessary codes removed from burn smart contract

--- public/controller/smart-contracts/template/Burn.hs
+++ public/controller/smart-contracts/template/Burn.hs
@@ -1,78 +1,30 @@
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE NoImplicitPrelude   #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE TypeApplications    #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
-module {{project_name}}.Burn where
+module Burn where
 
-import           Control.Monad       hiding (fmap)
-import           Data.Map            as Map
-import           Data.Text           (Text)
-import           Data.Void           (Void)
-import           Plutus.Contract
-import           PlutusTx            (Data (..))
-import qualified PlutusTx
-import qualified PlutusTx.Builtins   as Builtins
-import           PlutusTx.Prelude    hiding (Semigroup(..), unless)
-import           Ledger              hiding (singleton)
-import           Ledger.Constraints  as Constraints
-import qualified Ledger.Scripts      as Scripts
-import           Ledger.Ada          as Ada
-import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage)
-import           Playground.TH       (mkKnownCurrencies, mkSchemaDefinitions)
-import           Playground.Types    (KnownCurrency (..))
-import           Prelude             (IO, Semigroup (..), String)
-import           Text.Printf         (printf)
+import qualified Plutus.V2.Ledger.Api as PlutusV2
+import           PlutusTx             (BuiltinData, compile)
+import           PlutusTx.Prelude     (traceError)
+import           Prelude              (IO)
+import           Serialise             ( writeValidatorToFile)
 
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+---------------------------------------------------------------------------------------------------
+----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
 
-{-# INLINABLE mkValidator #-}
-mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-mkValidator _ _ _ = traceError "BURNT!"
+-- This validator always fails
+--                    Datum         Redeemer     ScriptContext
+mkBurnValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkBurnValidator _ _ _ = traceError "it burns!!!"
+{-# INLINABLE mkBurnValidator #-}
 
-validator :: Validator
-validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
+validator :: PlutusV2.Validator
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| mkBurnValidator ||])
 
-valHash :: Ledger.ValidatorHash
-valHash = Scripts.validatorHash validator
+---------------------------------------------------------------------------------------------------
+------------------------------------- HELPER FUNCTIONS --------------------------------------------
 
-scrAddress :: Ledger.Address
-scrAddress = scriptAddress validator
-
-type GiftSchema =
-            Endpoint "give" Integer
-        .\/ Endpoint "grab" ()
-
-give :: AsContractError e => Integer -> Contract w s e ()
-give amount = do
-    let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkConstr 0 []) $ Ada.lovelaceValueOf amount
-    ledgerTx <- submitTx tx
-    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-    logInfo @String $ printf "made a gift of %d lovelace" amount
-
-grab :: forall w s e. AsContractError e => Contract w s e ()
-grab = do
-    utxos <- utxosAt scrAddress
-    let orefs   = fst <$> Map.toList utxos
-        lookups = Constraints.unspentOutputs utxos      <>
-                  Constraints.otherScript validator
-        tx :: TxConstraints Void Void
-        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ Builtins.mkI 17 | oref <- orefs]
-    ledgerTx <- submitTxConstraintsWith @Void lookups tx
-    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-    logInfo @String $ "collected gifts"
-
-endpoints :: Contract () GiftSchema Text ()
-endpoints = awaitPromise (give' `select` grab') >> endpoints
-  where
-    give' = endpoint @"give" give
-    grab' = endpoint @"grab" $ const grab
-
-mkSchemaDefinitions ''GiftSchema
-
-mkKnownCurrencies []
+saveVal :: IO ()
+saveVal = writeValidatorToFile "./assets/burn.plutus" validator
